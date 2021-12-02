ad5ebeecacd0d2b4ba71b455d6c2c64f815ea647
smetanova-ys <smetanova.ys@gmail.com>
2021-12-02 23:54:31 +0100

    First test added 2

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,2 +1,29 @@
-package ru.netology.manager;public class MovieManager {
+package ru.netology.manager;
+
+import ru.netology.domain.MovieCard;
+
+public class MovieManager {
+
+    private MovieCard[] movies = new MovieCard[0];
+
+    public void add(MovieCard movie) {
+        int length = movies.length + 1;
+        MovieCard[] tmp = new MovieCard[length];
+
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public MovieCard[] getMovies() {
+        return movies;
+    }
+
+    public MovieManager() {
+    }
+
+
 }
--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -1,4 +1,28 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.MovieCard;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MovieManagerTest {
-  
+
+    @Test
+    public void shouldInspectAdd() {
+        MovieManager manager = new MovieManager();
+
+        MovieCard first = new MovieCard(1, 1, "first", "comedy");
+        MovieCard second = new MovieCard(2, 2, "second", "thriller");
+        MovieCard third = new MovieCard(3, 3, "third", "drama");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        MovieCard[] actual = manager.getMovies();
+        MovieCard[] expected = new MovieCard[] {first, second, third};
+
+        assertArrayEquals(expected, actual);
+    }
+
 }
